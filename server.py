405a371f0db033432cf04ce9e2d678f7b6ec2660
ahmedib395 <ahmedib5678@gmail.com>
2026-04-25 14:53:05 +0300

    Change executor to use raw_tac instead of opt_tac

--- server.py
+++ server.py
@@ -147,7 +147,7 @@ class CompilerHTTPRequestHandler(BaseHTTPRequestHandler):
                 except ValueError: pass
                 parsed.append(s)
 
-            executor = TACExecutor(opt_tac, stdin_values=parsed)
+           executor = TACExecutor(raw_tac, stdin_values=parsed)
             output   = executor.run()
             response["program_output"] = output if output.strip() else "(no output)"
         except Exception as e:
